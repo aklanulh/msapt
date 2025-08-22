e45eeddfe965d8adfa2b41e941f6e4e66bd1d71c
aklanulh <aklanulh@gmail.com>
2025-08-22 08:36:41 +0700

    Update controllers

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -13,9 +13,9 @@ class ContactController extends Controller
             'address' => 'Ruko Maison Avenue MA.19, Kota Wisata, Cibubur, Kabupaten Bogor, 16820',
             'whatsapp' => '0811 9466 470',
             'email' => 'mitrajayaselarasabadi@gmail.com',
-            'email_cs' => 'cs@ptmsa.biz.id',
-            'email_alt' => 'marketing@ptmsa.biz.id',
-            'website' => 'www.ptmsa.biz.id',
+            'email_cs' => '',
+            'email_alt' => '',
+            'website' => 'www.ptmsa.co.id',
             'office_hours' => 'Senin - Jumat: 08:00 - 17:00 WIB',
             'maps_embed' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.1234567890!2d106.9234567!3d-6.3456789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69abc123def456%3A0x789abc123def4567!2sRuko%20Maison%20Avenue%2C%20Kota%20Wisata%2C%20Cibubur!5e0!3m2!1sen!2sid!4v1234567890'
         ];
@@ -36,7 +36,7 @@ class ContactController extends Controller
 
         // In a real application, you would save to database and send email
         // For now, we'll just redirect with success message
-        
+
         return redirect()->route('contact')->with('success', 'Pesan Anda telah berhasil dikirim. Tim kami akan segera menghubungi Anda.');
     }
 }
