7001f4f1d02f758f6ea9899439d65520d52de0aa
aklanulh <aklanulh@gmail.com>
2025-08-26 11:08:54 +0700

    Update admin login credentials - username: ptmsaalkeslabbmhp, password: ptmsa112233

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create admin user with specific credentials
+        User::create([
+            'name' => 'PT MSA Alkes Lab Admin',
+            'email' => 'ptmsaalkeslabbmhp',
+            'password' => Hash::make('ptmsa112233'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
